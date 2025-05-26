c0fbf9dd25fd58b94a37b02e21a269bff2a5bd7a
Joaquin Gutierrez <Joaquin.Gutierrez@fresenius-kabi.com>
2025-05-26 11:57:37 -0400

    a

--- prueba.py
+++ prueba.py
@@ -1,4 +1,11 @@
 import os
 
 elementos = os.listdir()
+#ver elementos dentro de la carpeta data
+if 'data' in elementos:
+    elementos = os.listdir('data')
+else:
+    print("La carpeta 'data' no existe en el directorio actual.")
+    elementos = []
+    
 print(elementos)
